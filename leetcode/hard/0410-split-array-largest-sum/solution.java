1fd7afea86e39c7e84b551d4d196965902b732ab
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-05 09:05:55 +0530

    ✅ Split Array Largest Sum | Hard | Java | Runtime: 2 ms (31.12%) | Memory: 43 MB | 2026-09-05

---
+++ leetcode/hard/0410-split-array-largest-sum/solution.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int low=0;
+        int high=0;
+
+        int sum=0;
+        int max=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                max=nums[i];
+            }
+            sum+=nums[i];
+        }
+        low=max;
+        high=sum;
+        
+        while(low<high){
+            int mid=(low+high)/2;
+            int parts=1;
+            int currentsum=0;
+
+            for(int i=0;i<nums.length;i++){
+                if(currentsum+nums[i]>mid){
+                    parts++;
+                    currentsum=nums[i];
+                }
+                else{
+                    currentsum+=nums[i];
+                }
+            }
+            if(parts>k){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+}

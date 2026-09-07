afa95a8fdf09f91830a04b54b0ab0f5c529a84cb
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-07 09:28:36 +0530

    ✅ Aggressive Cows | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-07

---
+++ gfg/medium/aggressive-cows/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int aggressiveCows(int[] arr, int k) {
+       Arrays.sort(arr);
+       int low=1;
+       int high=(arr[arr.length-1]-arr[0]);
+    
+       int ans=0;
+       while(low<=high){
+           int mid=low+(high-low)/2;
+           if(canPlace(arr,k,mid)){
+               ans=mid;
+               low=mid+1;
+           }else{
+               high=mid-1;
+           }
+          
+       }
+        return ans;
+    }
+    private boolean canPlace(int []arr,int k , int dist){
+        int cows=1;
+        int prevCow=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]-prevCow>=dist){
+                cows++;
+                prevCow=arr[i];
+                
+            }
+           
+        }
+        if(cows>=k)return true;
+        else return false;
+    }
+}

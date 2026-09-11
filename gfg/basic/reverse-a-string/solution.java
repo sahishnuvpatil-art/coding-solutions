4e98fa34ad29c1f4a18e86f1d831142482834335
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-11 11:01:48 +0530

    ✅ Reverse a String | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ gfg/basic/reverse-a-string/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static String reverseString(String s) {
+        StringBuilder sb=new StringBuilder(s);
+        sb.reverse();
+        
+        return sb.toString();
+        
+    }
+}

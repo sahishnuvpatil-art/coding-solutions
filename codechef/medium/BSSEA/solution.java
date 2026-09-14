da6020b89af65875c2eded3436948e706f819de6
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-14 20:19:32 +0530

    ✅ BSSEA | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/BSSEA/solution.java
+++ codechef/medium/BSSEA/solution.java
@@ -9,6 +9,10 @@ class Codechef
       Scanner sc=new Scanner (System.in);
       int n=sc.nextInt();
       int []st=new int[n];
+      
+      for(int i=0;i<n;i++){
+          st[i]=sc.nextInt();
+      }
       Arrays.sort(st);
       int one=st[0];
       int last=st[st.length-1];

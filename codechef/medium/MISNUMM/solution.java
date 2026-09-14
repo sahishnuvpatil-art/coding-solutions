51ec2c7c424c5511718ca4f5ffffae33a1cde621
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-14 20:35:52 +0530

    ✅ MISNUMM | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/MISNUMM/solution.java
+++ codechef/medium/MISNUMM/solution.java
@@ -2,11 +2,65 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
+
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		
+	  
 
-	}
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+
+        int[] b = new int[m];
+
+        for (int i = 0; i < m; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> chutney = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+
+            int x = b[i];
+
+            int bhalu = 0;
+            int sher = 0;
+
+            for (int j = 0; j < m; j++) {
+                if (b[j] == x) {
+                    bhalu++;
+                }
+            }
+
+            for (int j = 0; j < n; j++) {
+                if (a[j] == x) {
+                    sher++;
+                }
+            }
+
+            if (bhalu > sher && !chutney.contains(x)) {
+                chutney.add(x);
+            }
+        }
+
+        Collections.sort(chutney);
+
+        if (chutney.size() == 0) {
+            System.out.println(-1);
+        } else {
+            for (int x : chutney) {
+                System.out.print(x + " ");
+            }
+        }
+    }
 }

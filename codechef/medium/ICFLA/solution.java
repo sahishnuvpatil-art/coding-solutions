84741f22761c52a81b963cb35b680031f7ee77bb
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-14 20:47:17 +0530

    ✅ ICFLA | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/ICFLA/solution.java
+++ codechef/medium/ICFLA/solution.java
@@ -6,7 +6,19 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		
+        Scanner sc = new Scanner(System.in);
 
-	}
+        long n = sc.nextLong();
+
+        long bhalu = 1;
+
+        while (bhalu * (bhalu - 1) / 2 < n) {
+            bhalu++;
+        }
+
+        System.out.println(bhalu);
+    }
 }
+
+	

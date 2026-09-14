6605907cb28c57088b1c50f6d3a27833c6f0ac5a
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-14 20:46:46 +0530

    ✅ TCTQU | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-14

--- codechef/medium/TCTQU/solution.java
+++ codechef/medium/TCTQU/solution.java
@@ -6,32 +6,7 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	
-        Scanner sc = new Scanner(System.in);
+		// your code goes here
 
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-
-        long[] t = new long[n];
-
-        for (int i = 0; i < n; i++) {
-            t[i] = sc.nextLong();
-        }
-
-        long target = t[k];
-        long time = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            if (i <= k) {
-                time += Math.min(t[i], target);
-            } else {
-                time += Math.min(t[i], target - 1);
-            }
-        }
-
-        System.out.println(time);
-    }
+	}
 }
-
-	

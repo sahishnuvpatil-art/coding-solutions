b60350fbe8d889738c668d41a7bbb877a076d893
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-15 08:20:10 +0530

    ✅ GPCK | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-15

--- codechef/medium/GPCK/solution.java
+++ codechef/medium/GPCK/solution.java
@@ -6,11 +6,6 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	    Scanner sc=new Scanner(System.in);
-	    int a=sc.nextInt();
-	    int b=sc.nextInt();
-	    if(a>b)System.out.print(b);
-	    else System.out.print(a);
 
 	}
 }

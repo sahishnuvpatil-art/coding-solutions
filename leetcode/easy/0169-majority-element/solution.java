6a317c5d702ee9658a5804c18b6df1faa5601c2c
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-15 08:48:34 +0530

    ✅ Majority Element | Easy | Java | Runtime: 7 ms (44.97%) | Memory: 63.1 MB | 2026-09-15

--- leetcode/easy/0169-majority-element/solution.java
+++ leetcode/easy/0169-majority-element/solution.java
@@ -1,21 +1,9 @@
 class Solution {
     public int majorityElement(int[] nums) {
-         int l=nums.length;
-        int newl=0;
-        int ans=0;
-        if(l%2==0){
-          newl=l/2;
-        }else{
-            newl=(l/2);
-        }
-        Arrays.sort(nums);
-        if(nums[l-1]==nums[newl]){
-           ans=nums[l-1];
-
-        }else{
-            ans=nums[0];
-        }
-        ans=ans*1;
-        return ans;
+         int l=nums.length/2;
+         Arrays.sort(nums);
+         int ans=nums[l];
+         return ans;
+        
     }
 }

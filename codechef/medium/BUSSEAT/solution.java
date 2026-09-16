a587ba062c7f8bfe2154b270f7ba5646d6327121
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-16 20:51:35 +0530

    ✅ BUSSEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/BUSSEAT/solution.java
+++ codechef/medium/BUSSEAT/solution.java
@@ -8,9 +8,15 @@ class Codechef
 	
 	{
 	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
 	   while(t-->0){
 	       int n=sc.nextInt();
 	       int k=sc.nextInt();
+	       if(n*2==k)System.out.println(k);
+	       else(n>=k)System.out.println("0");
+	       int num=0;
+	       
+	       
 	   }
 
 	}

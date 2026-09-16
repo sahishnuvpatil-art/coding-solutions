6e28f66fcfb7e8cf797eca9bc6c7705f099f6fc9
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-16 21:08:42 +0530

    ✅ REDBLUE7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

--- codechef/medium/REDBLUE7/solution.java
+++ codechef/medium/REDBLUE7/solution.java
@@ -6,7 +6,29 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		
+        Scanner sc = new Scanner(System.in);
 
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+
+            long sum = 0;
+            long max = 0;
+
+            for (int i = 0; i < N; i++) {
+                long x = sc.nextLong();
+
+                sum += x;
+                max = Math.max(max, x);
+            }
+
+            long answer = sum + (N - 2L) * max;
+
+            System.out.println(answer);
+        }
+
+       
 	}
 }

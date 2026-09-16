da8377cb2b100267c82cca12574f569b00db98e1
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-16 20:34:58 +0530

    ✅ BUSSEAT | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-16

---
+++ codechef/medium/BUSSEAT/solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	
+	{
+	    Scanner sc=new Scanner(System.in);
+	   while(t-->0){
+	       int n=sc.nextInt();
+	       int k=sc.nextInt();
+	   }
+
+	}
+}

8c396bdabd66c6296d8033d72de300789bb39dc7
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-21 08:26:10 +0530

    ✅ Linked List Insertion At Beginning | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ gfg/easy/linked-list-insertion-at-beginning/solution.java
@@ -0,0 +1,20 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtFront(Node head, int x) {
+        // code here
+        Node newNode=new Node(x);
+            newNode.next=head;
+               head=newNode;
+        return head;
+    }
+}

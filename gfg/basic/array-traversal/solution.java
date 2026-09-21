b51100190d6b6d2934ba7a0d6517a30228ebfa2d
sahishnu patil <sahishnuvpatil@gmail.com>
2026-09-21 08:43:17 +0530

    ✅ Array or List Traversal | Basic | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ gfg/basic/array-traversal/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public static void arrayTraversal(int[] arr) {
+        int l=arr.length;
+        for(int i=0;i<l;i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+}
